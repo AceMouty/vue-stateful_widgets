68102caae5f99fc16908875df9e2f5ffc50ab38e
Kyle G <kyleguerrero85@gmail.com>
2021-06-13 23:56:59 -0500

    chore: removed uneeded style tags

--- src/components/Moods.vue
+++ src/components/Moods.vue
@@ -33,9 +33,11 @@ STEPS 4, 5, 6:
 </template>
 
 <script>
+
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
+
 export default {
     name: 'Moods',
     data() {
@@ -60,7 +62,3 @@ export default {
     }
 }
 </script>
-
-<style>
-
-</style>
