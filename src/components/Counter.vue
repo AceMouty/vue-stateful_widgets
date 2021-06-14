20943ea89e91fe5cf762b1a481e1c1253bcece3d
Kyle G <kyleguerrero85@gmail.com>
2021-06-13 23:28:43 -0500

    setup: setup the Counter and Input components

---
+++ src/components/Counter.vue
@@ -0,0 +1,89 @@
+<!--
+Counter Instructions
+
+How many slices of data do you think are necessary to act as "sources of truth" for all
+the things that change in this widget? Give it some thought before continuing reading!
+
+STEP 0:
+  Start by studying the component below, and setup the data function / method.
+
+STEP 1:
+  In the **data** method, create a 'count' property.
+  The initial value should be set to 0.
+
+STEP 2:
+  The 'style' object in our getStyles method has the 'color' property hard-coded to "royalblue".
+  What the value of 'color' should be instead is a vlue changed dynamically, it goes like this:
+  If count is even, then "royalblue", else "crimson" and then return the styles object 
+
+STEP 3:
+  We need to replace some hard-coded info in the template with expressions, inside mustache syntax (double curly braces).
+  Start by replacing the character "0" with {{ count }}. The 'count' slice of data is the source of truth here.
+  Then, replace the word "even" with a ternary: {{ if count is even number, then string "even", else string "odd" }}.
+
+  Docs: https://v3.vuejs.org/guide/template-syntax.html#using-javascript-expressions
+
+STEP 4:
+  Increment Function
+
+  This click handler needs to set the 'count' to be the current 'count' plus one.
+
+STEP 5:
+  Decrement Function
+
+  This click handler needs to set the 'count' to be the current 'count' minus one.
+
+STEP 6:
+  Reset Function
+  This click handler needs to set the 'count' to be zero again.
+-->
+<template>
+  <div class='widget-counter container'>
+      <h2>Counter</h2>
+      <div id='count' :style="getStyles()">
+        Number 0 is even <!-- Step 3 --> 
+      </div>
+      <div>
+        <button id='increment' @click="increment">Increment</button>
+        <button id='decrement' @click="decrement">Decrement</button>
+        <button id='resetCount' @click="reset">Reset</button>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Counter",
+    data() {
+        return {
+          // Step 1
+
+        }
+    },
+    methods: {
+        getStyles() {
+
+            const styles = {
+              fontSize: '1.5em',
+              marginBottom: '0.3em',
+              color: 'royalblue' // Step 2 
+            }
+
+            return styles
+        },
+        increment() {
+            // Step 4
+        },
+        decrement() {
+            // Step 5
+        },
+        reset() {
+            // Step 6
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,66 @@
+<!--
+Input Instructions
+
+STEP 0:
+  Study the component below and get familiar with what is already setup.
+
+STEP 1:
+  Create a data property called 'inputValue' and set it to a empty string
+
+STEP 2:
+  Make the color of the text be crimson if the length of 'inputValue' goes over ten.
+
+STEP 3:
+  Interpolate the value of the input inside the div with an id of 'output'. How can we make it show in ALL CAPS?
+
+  Docs: https://v3.vuejs.org/guide/template-syntax.html#using-javascript-expressions
+
+STEP 4:
+  Using the v-model, bind to the 'input' data property on our input field that has the id of 'input'
+
+STEP 5:
+  Set the 'input' data attribute to be empty string. 
+
+-->
+<template>
+    <div class='widget-input container'>
+      <h2>Input</h2>
+      <div id='output' v-bind:style="getStyles()"></div> <!-- Step 3 -->
+      <div>
+        <input id='input' type='text' /> 
+        <button id='resetInput' v-on:click="reset">Reset</button>
+      </div>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    data() {
+        // Step 1
+        return {
+
+        }
+    },
+    methods: {
+        getStyles() {
+            const styles = {
+                fontSize: '1.5em',
+                marginBottom: '0.3em',
+                color: 'royalblue', /* STEP 2 */ 
+            }
+
+            return styles
+        },
+        reset() {
+            // Step 5
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
